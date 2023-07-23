e7e11670ab4ebab51f428400cd25c68a6c0d0728
sahil gaikwad <sahil.gaikwad@elastic.run>
2023-07-23 13:22:22 +0530

    UI changes and api changes

--- form/src/Form.js
+++ form/src/Form.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Form = () => {
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-  });
-
+    const initialFormData = {
+        username: '',
+        email: '',
+        password: '',
+      };
+  const [formData, setFormData] = useState(initialFormData);
+  const [isLogin, setIsLogin] = useState(true);
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -14,11 +17,15 @@ const Form = () => {
       [name]: value,
     }));
   };
+  const handleToggle = () => {
+    setIsLogin((prevIsLogin) => !prevIsLogin);
+  };
 
   const handleSubmit = async(e) => {
-    e.preventDefault();
+    e.preventDefault(); 
+
     
-    const response = await fetch('http://localhost:8080/hello', {
+    const response = await fetch('http://localhost:8080/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -27,17 +34,42 @@ const Form = () => {
       })
         
     const data = await response.json()
-    console.log(data)
+    console.log(data.status)
+    if(data.status=='green'){
+    toast.success(data.message, {
+        position: toast.POSITION.TOP_RIGHT, // You can customize the position of the toast
+        autoClose: 3000, // Time in milliseconds after which the toast will be automatically closed
+        hideProgressBar: false, // Show or hide the progress bar
+      });
+      setFormData(initialFormData);
+    }
+    else{
+        toast.error(data.message, {
+            position: toast.POSITION.TOP_RIGHT, // You can customize the position of the toast
+            autoClose: 3000, // Time in milliseconds after which the toast will be automatically closed
+            hideProgressBar: false, // Show or hide the progress bar
+          });
+        }
+    // else{
+    //     toast.error(data.message, {
+    //         position: toast.POSITION.TOP_RIGHT, // You can customize the position of the toast
+    //         autoClose: 3000, // Time in milliseconds after which the toast will be automatically closed
+    //         hideProgressBar: false, // Show or hide the progress bar
+    //       });
+    //     }
+
+    // }
     // Here, you can handle form submission logic, e.g., send data to a server.
     // console.log(formData);
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ">
+    <div className="min-h-screen flex items-center justify-center ">
       <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
-        <h2 className="text-3xl font-semibold text-center mb-6">Sign Up</h2>
+        <h2 className="text-3xl font-bold billabong-font text-center mb-6">{isLogin ? 'Login' : 'Sign Up'}</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
+        {!isLogin && (<div>
             <input
               type="text"
               id="username"
@@ -48,7 +80,7 @@ const Form = () => {
               className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
               required
             />
-          </div>
+          </div>)}
           <div>
             <input
               type="email"
@@ -76,12 +108,20 @@ const Form = () => {
           </div>
           <button
             type="submit"
-            className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300"
+            className="w-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300"
           >
-            Sign Up
+           {!isLogin ? 'Sign Up' : 'Login'}
           </button>
         </form>
+        <p>
+        {isLogin ? "Don't have an account? " : 'Already have an account? '}
+        <button type="button" onClick={handleToggle}>
+          {isLogin ? 'Sign Up' : 'Login'}
+        </button>
+      </p>
       </div>
+    </div>
+    <ToastContainer />
     </div>
   );
 };
--- go/main.go
+++ go/main.go
@@ -15,13 +15,18 @@ type UserData struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+type ResponseData struct {
+    Status string `json:"data"`;
+	Message string `json:"data"`;
+	Data string `json:"data"`
+}
 
 func main() {
 	router := gin.Default()
 	router.Use(corsMiddleware())
 	// router.Use(cors.Default())
 	// Define your API endpoints here
-	router.POST("/hello", helloHandler)
+	router.POST("/signup", helloHandler)
 
 	// Run the server on port 8080
 	router.Run(":8080")
@@ -32,10 +37,9 @@ func helloHandler(c *gin.Context) {
 	var userData UserData;
 	c.ShouldBindJSON(&userData)
 	fmt.Println(userData.Username)
-	openconn(userData.Username,userData.Email,userData.Password)
-	c.JSON(200, gin.H{
-		"message": "Hello, World!",
-	})
+	resp:=openconn(userData.Username,userData.Email,userData.Password)
+	fmt.Println(resp.Status)
+	c.JSON(200, gin.H{"status":resp.Status,"message":resp.Message})
 }
 
 
@@ -47,7 +51,7 @@ const (
 	dbName     = "gin"
 )
 
-func openconn(username,email,password string) {
+func openconn(username,email,password string) ResponseData{
 	// Open a connection to the database
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@/%s", dbUser, dbPassword, dbName))
 	if err != nil {
@@ -56,24 +60,66 @@ func openconn(username,email,password string) {
 	defer db.Close()
 
 	// Insert data into the users table
-	insertData(db, username, email,password)
+	return insertData(db, username, email,password)
+	
 }
 
-func insertData(db *sql.DB, username, email, password string) {
+func insertData(db *sql.DB, username, email, password string) ResponseData {
 	// Prepare the SQL statement for inserting data
-	stmt, err := db.Prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)")
-	if err != nil {
-		log.Fatalf("Error preparing SQL statement: %v", err)
-	}
-	defer stmt.Close()
+	// stmt, err := db.Prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)")
+	// if err != nil {
+	// 	log.Fatalf("Error preparing SQL statement: %v", err)
+	// }
+	// defer stmt.Close()
 
-	// Execute the SQL statement to insert data
-	_, err = stmt.Exec(username, email, password)
-	if err != nil {
-		log.Fatalf("Error inserting data: %v", err)
+	// // Execute the SQL statement to insert data
+	// _, err = stmt.Exec(username, email, password)
+	// if err != nil {
+	// 	log.Fatalf("Error inserting data: %v", err)
+	// }
+
+	// log.Println("Data inserted successfully.")
+	var userCount int
+    query := "SELECT COUNT(*) FROM users WHERE email = ?"
+    err := db.QueryRow(query,email).Scan(&userCount)
+    if err != nil {
+		return ResponseData{
+			Status:"red",
+			Message: "Database query error",
+		}
+		// return gin.H{"error": "Database query error"}
+        // c.JSON(http.StatusInternalServerError, gin.H{"error": "Database query error"})
+        // return
+    }
+
+    // Insert or throw an error based on user existence
+    if userCount > 0 {
+		return ResponseData{
+			Status:"red",
+			Message: "User already exists",
+		}
+		// return gin.H{"error": "User already exists"}
+        // c.JSON(http.StatusConflict, gin.H{"error": "User already exists"})
+        // return
+    }
+
+    // User does not exist, insert the user
+    insertQuery := "INSERT INTO users (username, email,password) VALUES (?, ?, ?)"
+    _, err = db.Exec(insertQuery, username, email, password)
+    if err != nil {
+		return ResponseData{
+			Status:"red",
+			Message: "Database insert error",
+		}
+        // c.JSON(http.StatusInternalServerError, gin.H{"error": "Database insert error"})
+        // return
+    }
+	return ResponseData{
+		Status:"green",
+		Message: "User added successfully",
 	}
 
-	log.Println("Data inserted successfully.")
+    // c.JSON(http.StatusOK, gin.H{"message": "User added successfully"})
 }
 func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
